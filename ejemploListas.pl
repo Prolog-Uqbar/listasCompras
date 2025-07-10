649c0662371b010c8a978d7c641d0de4528a8aa9
Lucas Spigariol <lspigariol@gmail.com>
2025-07-10 12:37:08 -0300

    Add files via upload

--- ejemploListas.pl
+++ ejemploListas.pl
@@ -3,23 +3,40 @@ precio(buffet, cafe,2000).
 precio(buffet, medialuna,500).
 precio(kiosco, agua,1200).
 precio(kiosco, gaseosa,1700).
-precio(buffet, agua,1300).
+precio(buffet, agua,300).
 precio(resto, agua,2300).
 precio(resto, cafe,5000).
-precio(resto, medialuna,300).
+precio(resto, medialuna,3000).
 precio(resto, mila,10000).
-precio(carni, bondiola,5000).
+precio(carni, bondiola,500).
 precio(carni, grasa,900).
 
-negocioVendePromedio(Negocio,Promedio):-
+negocioVendeBaratoPromedio(Negocio):-
+    precio(Negocio,_,_),
+    not(negocioVendeCaroPromedio(Negocio)).
+
+negocioVendeCaroPromedio(Negocio):-
     precio(Negocio,_,_),
     findall(Precio, precio(Negocio,_,Precio) ,ListaPrecios),
     length(ListaPrecios, Cantidad),
     sumlist(ListaPrecios, Total),
-    Promedio is Total / Cantidad.
+    Promedio is Total / Cantidad,
+    Promedio > 1000.
    
+vendeMayoriaCaro(Negocio) :-
+    cantidadProductosVendidos(Negocio,CantidadTotal),
+    cantidadProductosCaros(Negocio,CantidadCaro),
+    CantidadCaro > CantidadTotal / 2.
 
+cantidadProductosCaros(Negocio,CantidadCaro):-
+    precio(Negocio, _, _),
+    findall(_, vendeAlgoCaro(Negocio), ListaPreciosCaros),
+    length(ListaPreciosCaros, CantidadCaro).
 
+cantidadProductosVendidos(Negocio,CantidadTotal):-
+    precio(Negocio, _, _),
+    findall(_, precio(Negocio, _ ,_), ListaPreciosTotal),
+    length(ListaPreciosTotal, CantidadTotal).
 
 preciosVigentes(kiosco,[4000,6000,300,700]).
 preciosVigentes(resto,[300,700]).
@@ -41,11 +58,21 @@ promedioNegocio(Negocio,Promedio):-
     
 
 
-algoMasCaroQueOtro(Negocio, OtroNegocio):-
+algoMasCaroQueOtro(Negocio, OtroNegocio,Producto):-
     precio(Negocio, Producto, Precio1),
     precio(OtroNegocio, Producto, Precio2),
     OtroNegocio \= Negocio,
     Precio1 > Precio2.
+
+vendeAlgoMasCaro(N1,N2,ProductosMasCarosN1):-
+    precio(N1,_,_),
+    precio(N2,_,_),
+    findall(
+        Pr,
+        (precio(N1,Pr,Precio1), precio(N2,Pr,Precio2),Precio1>Precio2),
+        ProductosMasCarosN1
+    ).
+
 
 vendeTodoCaro(Negocio):- %todos mas que 1000
     precio(Negocio,_,_),
@@ -90,3 +117,16 @@ cantidadNegociosCafeteros(C):-
 
 
 
+
+vendeMayoriaMasCaroQue(Negocio,OtroNegocio) :-
+    cantidadProductosVendidos(Negocio,CantidadTotal),
+    cantidadProductosMasCarosQue(Negocio,OtroNegocio,CantidadCaro),
+    CantidadCaro > CantidadTotal / 2.
+    
+cantidadProductosMasCarosQue(Negocio,OtroNegocio,CantidadCaro):-
+    precio(Negocio, _, _),
+    precio(OtroNegocio, _, _),
+    findall(_, algoMasCaroQueOtro(Negocio,OtroNegocio,_), ListaPreciosCaros),
+    length(ListaPreciosCaros, CantidadCaro).
+    
+    
